507197cfa16497430c29fd91d0ca4df4d1e16d8a
Artem Lutsenko <artem.luceno@gmail.com>
2021-03-25 16:18:47 +0200

    add depenent for associations

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,8 +1,8 @@
 class TodoList < ApplicationRecord
-  has_many :tasks
-  has_many :invites
+  has_many :tasks, dependent: :delete_all
+  has_many :invites, dependent: :delete_all
   has_many :members, through: :invites, source: :user
-  has_many :comments
+  has_many :comments, dependent: :delete_all
   belongs_to :user
 
   validates :title, presence: true
