3c3a18dbc8e0b4d55b53e2a4b11729d99a0a92a0
Artem Lutsenko <artem.luceno@gmail.com>
2021-03-25 06:45:19 +0200

    add bootstrap css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import * as mdb from 'mdb-ui-kit';
+import 'mdb-ui-kit/js/mdb.min'
 
 Rails.start()
 Turbolinks.start()
