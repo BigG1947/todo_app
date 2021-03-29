0f22b08fabe477d488e7138d43a8b11c4806e853
Artem Lutsenko <artem.luceno@gmail.com>
2021-03-29 03:52:41 +0300

    fix models

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -3,16 +3,17 @@ class Invite < ApplicationRecord
   belongs_to :todo_list
 
   validates :todo_list_id, :user_id, presence: true
-  validate :it_is_todo_list_owner
+  validates :user_id, uniqueness: {scope: :todo_list_id, message: 'this user already have invite for this todo list'}
+  validate :it_is_todo_list_owner, if: :todo_list_id
 
   has_secure_token :invite_token, length: 36
 
-  after_create_commit :send_invite_email
+  after_create :send_invite_email
 
   private
 
   def it_is_todo_list_owner
-    errors.add(:user, 'This user is todo list owner') if user.todo_lists.exists? id: todo_list.id
+    errors.add(:user, 'This user is todo list owner') if user&.todo_lists&.exists? id: todo_list.id
   end
 
   def send_invite_email
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,5 @@ class Task < ApplicationRecord
   belongs_to :todo_list
   belongs_to :priority
 
-  validates :title, :deadline, presence: true
+  validates :title, :deadline, :priority_id, presence: true
 end
